266bbc58f8c45e079a9649ca325553f535a4c30b
Ramiro Cosa <ramirocosa@gmail.com>
2023-04-13 06:13:24 -0300

    feat(frontend): home wireframe

--- package/frontend/src/app/services/resources.service.ts
+++ package/frontend/src/app/services/resources.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ResourcesService {
+  private url = 'http://localhost:3000/topic';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<any[]> {
+    return this.http.get<any[]>(this.url);
+  }
 }
